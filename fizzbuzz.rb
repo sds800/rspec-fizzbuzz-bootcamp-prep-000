4336f9a615ccf02e75b94d017d8e06a138fffec1
SDS 800 <jeremyhyman@gmail.com>
2018-03-01 19:28:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,10 @@ def fizzbuzz(int)
 
     elsif int % 5 == 0
       "Buzz"
-      elsif int % 15 == 0
-        "FizzBuzz"
-      else int
+    elsif int % 15 == 0
+      "FizzBuzz"
+
+    else int
         nil
 
   end
